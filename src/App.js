4b10971f9e3b6b660938fa9c20c92173bca601f0
sakhawathossensadi <newtonss919@gmail.com>
2022-09-12 22:28:28 +0600

    add Registration and Login page to Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Registration from './page/Registration/Registration';
+import Login from './page/Login/Login';
+// import Dashboard from './page/Dashboard/Dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Registration />} />
+        <Route path="/login" element={<Login />}/>
+        {/* <Route path="/dashboard" element={<Dashboard />}/> */}
+    </Routes>
+  </BrowserRouter>
   );
 }
 
