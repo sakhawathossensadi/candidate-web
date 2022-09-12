0bce00bf5ec65ad6f146a3126ddc985af27c485b
sakhawathossensadi <newtonss919@gmail.com>
2022-09-12 22:30:42 +0600

    api endpoints file

---
+++ src/config/endpoints.js
@@ -0,0 +1,4 @@
+export const SERVER_TALENT_ADMIN_CANDIDATES_ENDPOINT = '/admin/candidates';
+export const SERVER_TALENT_ADMIN_CANDIDATE_STATUS_UPDATE_ENDPOINT = '/admin/candidates/:candidateId/status';
+export const SERVER_TALENT_ADMIN_PROFILE_ENDPOINT = '/admin/profile';
+export const SERVER_TALENT_ADMIN_LOGOUT_ENDPOINT = '/auth/logout';
