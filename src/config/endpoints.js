b75b7d1d6bd3d5d463c3ddc3af02bd531ff37ea9
sakhawathossensadi <newtonss919@gmail.com>
2022-09-22 00:40:50 +0600

    api for candidate profile

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -3,7 +3,7 @@ const { REACT_APP_BASE_API } = process.env;
 
 export const SERVER_CANDIDATE_REGISTRATION_ENDPOINT = `${REACT_APP_BASE_API}/candidate/register`;
 export const SERVER_CANDIDATE_LOGIN_ENDPOINT = `${REACT_APP_BASE_API}/oauth/token`;
-export const SERVER_TALENT_ADMIN_PROFILE_ENDPOINT = `${REACT_APP_BASE_API}/candidate/profile`;
+export const SERVER_TALENT_CANDIDATE_PROFILE_ENDPOINT = `${REACT_APP_BASE_API}/candidate/profile`;
 // export const SERVER_TALENT_ADMIN_CANDIDATES_ENDPOINT = '/admin/candidates';
 export const SERVER_TALENT_ADMIN_CANDIDATE_STATUS_UPDATE_ENDPOINT = '/admin/candidates/:candidateId/status';
 export const SERVER_TALENT_ADMIN_LOGOUT_ENDPOINT = '/auth/logout';
